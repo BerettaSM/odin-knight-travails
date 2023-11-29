7ddeed699d4e2d8d2f5eb3b2d571f0610700eb99
BerettaSM <ramonsaviato@hotmail.com>
2023-11-29 00:34:47 -0300

    feat: implementation conclusion

--- knight.rb
+++ knight.rb
@@ -1,26 +1,81 @@
-$move_offsets = [
-    # x, y
-    [-1, -2], [1, -2],
-    [-2, -1], [2, -1],
-    [-2,  1], [2,  1],
-    [-1,  2], [1,  2],
-]
+class Knight
+    @@moves = [
+        # x, y
+        [-1, -2], [1, -2],
+        [-2, -1], [2, -1],
+        [-2,  1], [2,  1],
+        [-1,  2], [1,  2],
+    ]
 
-def is_within_board(x, y)
-    x.between?(0,7) && y.between?(0,7)
+    @@visited = Set.new
+
+    def self.reset
+        @@visited = Set.new
+    end
+    
+    attr_accessor :position, :parent
+
+    def initialize position, parent=nil
+        @position = position
+        @parent = parent
+        mark_as_visited(position)
+    end
+
+    def valid_moves
+        x, y = @position
+
+        @@moves.collect do |offset_x, offset_y|
+            [x + offset_x, y + offset_y]
+        end.filter do |new_x, new_y|
+            is_valid_move?(new_x, new_y) && !visited?([new_x, new_y])
+        end.collect do |move|
+            Knight.new(move, self)
+        end
+    end
+
+    def visited? position
+        position_id = get_id(position)
+        @@visited.include?(position_id)
+    end
+
+    def visited_positions
+        @@visited.clone
+    end
+
+    def path
+        cur = self
+        positions = []
+        until cur.nil?
+            positions.push(cur.position)
+            cur = cur.parent
+        end
+        positions.reverse
+    end
+
+    private
+
+    def is_valid_move? x, y
+        x.between?(0,7) && y.between?(0,7)
+    end
+
+    def mark_as_visited position
+        position_id = get_id(position)
+        @@visited.add(position_id)
+    end
+
+    def get_id position
+        x, y = position
+        "#{x},#{y}"
+    end
 end
 
-def get_valid_jumps(x, y, offsets=$move_offsets)
-    offsets.filter_map { |o_x, o_y| [x + o_x, y + o_y] if is_within_board(o_x + x, o_y + y) }
+def knight_moves(from, to)
+    Knight.reset
+    moves = [ Knight.new(from) ]
+
+    until moves.empty?
+        knight = moves.shift
+        return knight.path if knight.position == to
+        knight.valid_moves.each { |new_move| moves.push(new_move)}
+    end
 end
-
-def get_board
-    board = Array.new(8) { Array.new(8, false) }
-end
-
-def knight_moves(from, to, path=[], board=get_board)
-    p board
-end
-
-
-p get_valid_jumps(1, 1)
